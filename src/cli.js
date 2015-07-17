8e648c69b3872f84884322473e183859d205be27
Christoph Witzko <github@christophwitzko.com>
2015-07-17 17:52:05 +0200

    feat(cli): read loglevel from npmconf

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ const { readFileSync, writeFileSync } = require('fs')
 
 const async = require('async')
 const nopt = require('nopt')
+const npmconf = require('npmconf')
 const request = require('request')
 const Logger = require('./logger')
 
@@ -11,51 +12,52 @@ const github = require('./lib/github')
 const ci = require('./lib/ci')
 
 const knownOpts = {
-  debug: Boolean
+  next: Boolean
 }
 
 const shortHands = {
-  d: [ 'debug' ]
+  n: [ 'next' ]
 }
 
 export default function (argv) {
   let infoObj = {}
   infoObj.parsedArgs = nopt(knownOpts, shortHands, argv, 2)
-  // TODO: get log level from cli args
-  infoObj.loglevel = 'silly'
-  const log = infoObj.logger = new Logger(infoObj.loglevel)
-  log.verbose('Reading `package.json`.')
-  let pkg
-  try {
-    pkg = JSON.parse(readFileSync('package.json', 'utf8'))
-  } catch (e) {
-    log.error('Could not read/parse `package.json`.')
-    log.error('Please run `npm init`.')
-    log.silly(e)
-    return
-  }
-  async.applyEachSeries([
-    repository,
-    npm,
-    github,
-    ci
-  ], pkg, infoObj, (error) => {
-    if (error) return log.silly(error)
-    delete pkg.version
-    pkg.scripts['semantic-release'] = 'semantic-release-scripts pre && npm publish && semantic-release-scripts post'
-    request({
-      url: 'https://registry.npmjs.org/semantic-release-scripts',
-      json: true
-    }, (err, res, body) => {
-      if (err) {
-        log.error('Could not get latest `semantic-release-scripts` version.')
-        log.silly(err)
-      } else {
-        pkg.devDependencies['semantic-release-scripts'] = `^${body['dist-tags'].latest}`
-      }
-      log.verbose('Writing `package.json`.')
-      writeFileSync('package.json', `${JSON.stringify(pkg, null, 2)}\n`)
-      log.info('Done.')
+  npmconf.load((error, conf) => {
+    infoObj.loglevel = conf.get('loglevel')
+    const log = infoObj.logger = new Logger(infoObj.loglevel)
+    log.verbose('Reading `package.json`.')
+    let pkg
+    try {
+      pkg = JSON.parse(readFileSync('package.json', 'utf8'))
+    } catch (e) {
+      log.error('Could not read/parse `package.json`.')
+      log.error('Please run `npm init`.')
+      log.silly(e)
+      return
+    }
+    async.applyEachSeries([
+      repository,
+      npm,
+      github,
+      ci
+    ], pkg, infoObj, (error) => {
+      if (error) return log.silly(error)
+      delete pkg.version
+      pkg.scripts['semantic-release'] = 'semantic-release-scripts pre && npm publish && semantic-release-scripts post'
+      request({
+        url: 'https://registry.npmjs.org/semantic-release-scripts',
+        json: true
+      }, (err, res, body) => {
+        if (err) {
+          log.error('Could not get latest `semantic-release-scripts` version.')
+          log.silly(err)
+        } else {
+          pkg.devDependencies['semantic-release-scripts'] = `^${body['dist-tags'].latest}`
+        }
+        log.verbose('Writing `package.json`.')
+        writeFileSync('package.json', `${JSON.stringify(pkg, null, 2)}\n`)
+        log.info('Done.')
+      })
     })
   })
 }
