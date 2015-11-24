7d6bd30538b084b99ee399a07024824d7612f674
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-23 22:59:16 -0500

    fix(travis): use module with user-agent to comply with Travis CI rules
    Closes #69

--- src/lib/travis.js
+++ src/lib/travis.js
@@ -172,7 +172,10 @@ module.exports = function (endpoint, pkg, info, cb) {
   }
 
   const travis = info.travis = new Travis({
-    version: '2.0.0'
+    version: '2.0.0',
+    headers: {
+      'user-agent': 'Oghliner'
+    }
   })
   travis.agent._endpoint = endpoint
 
