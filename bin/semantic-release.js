c9d898e09566a4110b75f6c1dbcb93d4b9764fcf
Matt Travi <programmer@travi.org>
2023-12-08 14:46:49 -0600

    fix(babel): migrated babel dependencies to scoped versions

--- bin/semantic-release.js
+++ bin/semantic-release.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-/* eslint-disable import/no-unassigned-import */
-require('babel-register')({only: 'semantic-release-cli/src'});
-require('babel-polyfill');
+require('@babel/register')({only: 'semantic-release-cli/src'});
+require('@babel/polyfill');
 
 require('../src')().catch(() => {
   process.exitCode = 1;
